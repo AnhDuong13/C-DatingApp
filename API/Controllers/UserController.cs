7030036a17b777f4162ee0d66924a0dcf42e8b92
anhduong13 <junkstreet1712@gmail.com>
2022-05-31 15:49:56 +0700

    Sec 8

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Dtos;
 using API.Enities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,20 +16,24 @@ namespace API.Controllers
     [Authorize]
     public class UserController : BaseApiController
     {
-        private readonly DataContext _context;
-        public UserController(DataContext context)
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+        public UserController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            _mapper = mapper;
+            _userRepository = userRepository;
         }
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
         {
-            return await _context.Users.ToListAsync();
+            var users = await _userRepository.GetMembersAsync();
+            
+            return Ok(users);
         }
-        [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUsers(int id)
+        [HttpGet("{username}")]
+        public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
-            return await _context.Users.FindAsync(id);
+            return await _userRepository.GetMemberAsync(username);
         }
     }
 }
--- API/Enities/AppUser.cs
+++ API/Enities/AppUser.cs
@@ -14,7 +14,7 @@ namespace API.Enities
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public string KnowAs { get; set; }
+        public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
         public DateTime LastActive { get; set; } = DateTime.Now;
         public string Gender { get; set; }
@@ -25,9 +25,9 @@ namespace API.Enities
         public string Country { get; set; }
         public ICollection<Photo> Photos { get; set; }
 
-        public int GetAge()
-        {
-            return  DateOfBirth.CalculateAge();
-        }
+        // public int GetAge()
+        // {
+        //     return  DateOfBirth.CalculateAge();
+        // }
     }
 }
